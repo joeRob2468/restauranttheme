c2595ead58793108b87fb8547742693519259298
John Boyer <joeRob2468@yahoo.com>
2016-10-05 17:02:16 +0000

    installed and configured grunt

---
+++ gruntfile.js
@@ -0,0 +1,64 @@
+/** gruntfile.js */
+module.exports = function(grunt) {
+    grunt.initConfig({
+        pkg: grunt.file.readJSON('package.json'),
+
+        sass: {
+            options: {
+                /** Only use include_paths if extracting elements from Bower */
+                includePaths: ['themes/restaurant-theme/assets/vendor/foundation-sites/assets']
+            }, //options
+            dist: {
+                options: {
+                    outputStyle: 'expanded',
+                    sourceMap: false
+                },
+                files: {
+                    'themes/restaurant-theme/assets/css/main.css': 'themes/restaurant-theme/assets/scss/main.scss'
+                }
+            }
+        }, // sass
+
+        watch: {
+            options: {
+                livereload: true,
+                dateFormat: function(time) {
+                        grunt.log.writeln('The watch finished in ' + time + 'ms at ' + (new Date()).toString());
+                        grunt.log.writeln('Waiting for more changes...');
+                    } //date format function
+            }, //options
+            scripts: {
+                files: ['themes/**/*.js']
+            }, // scripts
+            //Live Reload of SASS
+            sass: {
+                files: ['themes/**/*.scss'],
+                tasks: ['sass']
+            }, //sass
+            css: {
+                files: ['themes/**/*.css', '!themes/**/main.css']
+            },
+            html: {
+                files: ['themes/**/*.html']
+            }, //html
+        }, //watch
+
+        postcss: {
+            options: {
+                processors: [
+                    require('autoprefixer')({
+                        browsers: 'last 2 versions'
+                    })
+                ]
+            }
+        }, //post css
+    });
+
+    grunt.loadNpmTasks('grunt-sass');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-autoprefixer');
+    grunt.loadNpmTasks('grunt-postcss');
+
+    grunt.registerTask('default', ['sass', 'watch']);
+};
+     
