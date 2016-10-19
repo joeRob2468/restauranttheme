b8dd4f6bccfaba0f6477ac64f0611fe646db1b2f
John Boyer <joeRob2468@yahoo.com>
2016-10-19 17:19:40 +0000

    added object-fit polyfill

--- themes/restaurant-theme/assets/js/main.js
+++ themes/restaurant-theme/assets/js/main.js
@@ -4,26 +4,29 @@ var scripts =
   // ready function. convenient for running scripts. 
   onReady: function()
   {
-    // initialize skrollr if we're not on mobile
-    if($('.parallax') && !(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera))
-    {
-      var s = skrollr.init({
-        forceHeight: false,
-        smoothScrolling: false
-      });
-    }
+    // initialize object-fit polyfill
+    objectFitImages('img.cover');
 
-    smoothScroll.init({
-      selector: 'a', // Selector for links (must be a class, ID, data attribute, or element tag)
-      selectorHeader: '[data-scroll-header]', // Selector for fixed headers (must be a valid CSS selector)
-      speed: 750, // Integer. How fast to complete the scroll in milliseconds
-      easing: 'easeInOutCubic', // Easing pattern to use
-      offset: 50, // Integer. How far to offset the scrolling anchor location in pixels
-      updateURL: true, // Boolean. If true, update the URL hash on scroll
-      callback: function ( anchor, toggle ) {} // Function to run after scrolling
-    });
+    // // initialize skrollr if we're not on mobile
+    // if($('.parallax') && !(/Android|iPhone|iPad|iPod|BlackBerry|Windows Phone/i).test(navigator.userAgent || navigator.vendor || window.opera))
+    // {
+    //   var s = skrollr.init({
+    //     forceHeight: false,
+    //     smoothScrolling: false
+    //   });
+    // }
+
+    // smoothScroll.init({
+    //   selector: 'a', // Selector for links (must be a class, ID, data attribute, or element tag)
+    //   selectorHeader: '[data-scroll-header]', // Selector for fixed headers (must be a valid CSS selector)
+    //   speed: 750, // Integer. How fast to complete the scroll in milliseconds
+    //   easing: 'easeInOutCubic', // Easing pattern to use
+    //   offset: 50, // Integer. How far to offset the scrolling anchor location in pixels
+    //   updateURL: true, // Boolean. If true, update the URL hash on scroll
+    //   callback: function ( anchor, toggle ) {} // Function to run after scrolling
+    // });
   },
 };
 
 $(document).foundation();
-// $(document).ready(scripts.onReady);
+$(document).ready(scripts.onReady);
