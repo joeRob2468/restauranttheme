5443a82daed810572b517fda23ecdf1696d45beb
John Boyer <joeRob2468@yahoo.com>
2018-04-12 12:35:39 -0500

    added lity and made some theme changes

--- config/database.php
+++ config/database.php
@@ -58,7 +58,7 @@ return [
             'port'      => 3306,
             'database'  => 'restauranttheme',
             'username'  => 'root',
-            'password'  => 'secret',
+            'password'  => 'root',
             'charset'   => 'utf8',
             'collation' => 'utf8_unicode_ci',
             'prefix'    => '',
